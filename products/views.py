f1cc0651c1e963bc470707e90c16318594244e88
AndyL86 <alinehan89@outlook.com>
2023-03-12 16:12:29 +0000

    fix product detail render issue and migrate

--- products/views.py
+++ products/views.py
@@ -17,10 +17,10 @@ def all_products(request):
 def product_details(request, product_id):
     """ A view to show all  products, including sorting and  search queries """
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_details.html', context)
