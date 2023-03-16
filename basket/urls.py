fdd9daeb02655684424f8f9d4991987788987193
AndyL86 <alinehan89@outlook.com>
2023-03-16 14:29:59 +0000

    add update and delete functionality to basket

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('edit/<item_id>/', views.edit_basket, name='edit_basket'),
+    path('remove/<item_id>/', views.remove_item, name='remove_item'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -21,3 +21,37 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def edit_basket(request, item_id):
+    """ Edit a quantity of a product in the shopping basket """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+def remove_item(request, item_id):
+    """
+    Remove entire quantity of a given product.
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    basket = request.session.get('basket', {})
+    quantity = basket[item_id] - 10
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    if not basket:
+        return redirect(reverse('basket'))
+    return redirect(reverse('basket'))
