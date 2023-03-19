503e203b90b3c9d0adeb6ccad528ace563b269d6
AndyL86 <alinehan89@outlook.com>
2023-03-19 15:33:15 +0000

    add checkout view and basic template

--- throwback/settings.py
+++ throwback/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'throwback.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
