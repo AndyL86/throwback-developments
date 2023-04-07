9552877e8ad7dc41c7f3f814dceda6849b367f0d
AndyL86 <alinehan89@outlook.com>
2023-04-07 21:06:12 +0000

    add footer and create featured products for home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.all()
+    featured_products = list(Product.objects.filter(featured=True).order_by(
+        'featured'))[-5:-1][::-1]  # show 4 recently featured products
+    print(featured_products)
+    context = {
+        'products': products,
+        'featured_products': featured_products,
+    }
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
