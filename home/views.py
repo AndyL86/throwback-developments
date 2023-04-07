e6329494dc35acdf8aacc0a609a63a7e4ddd305b
AndyL86 <alinehan89@outlook.com>
2023-04-07 23:16:36 +0000

    update product view

--- home/views.py
+++ home/views.py
@@ -9,7 +9,6 @@ def index(request):
     products = Product.objects.all()
     featured_products = list(Product.objects.filter(featured=True).order_by(
         'featured'))[-4:-1][::-1]  # show 4 recently featured products
-    print(featured_products)
     context = {
         'products': products,
         'featured_products': featured_products,
